368077dee5d27f078fb2491cf8b9cfe18615aa15
Elias700 <elias_ribeiro07@outlook.com.br>
2024-11-30 19:38:23 -0300

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { mainComponent } from './components/main/main.component';
+import { FooterComponent } from "./components/footer/footer.component";
 
 
 @Component({
@@ -14,6 +15,7 @@ import { mainComponent } from './components/main/main.component';
     HeaderComponent,
     mainComponent,
     CommonModule,
+    FooterComponent
 ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']  
